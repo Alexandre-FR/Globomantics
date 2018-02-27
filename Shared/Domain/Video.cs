671054e4105ecad14579a3199f2aef6de13289ae
Alexandre <36512175+Alexandre-FR@users.noreply.github.com>
2018-02-27 14:05:26 +0100

    1
    1

---
+++ Shared/Domain/Video.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Shared.Domain
+{
+   public class Video
+   {
+      public int Id { get; }
+
+      public string Title { get; }
+
+      public Genre Genres { get; }
+
+      public TimeSpan RunningTime { get; }
+
+      public double Rating { get; }
+
+      public double Rating2 { get; }
+
+      public Video(int id, string title, Genre genres, TimeSpan runningTime, double rating)
+      {
+         Id = id;
+         Title = title;
+         Genres = genres;
+         RunningTime = runningTime;
+         Rating = rating;
+      }
+   }
+}
